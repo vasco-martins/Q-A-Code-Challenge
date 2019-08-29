0a6e69fe4c978845a49f2b5fbce4393ee329f812
Vasco Martins <vascomartins37@gmail.com>
2019-08-29 19:27:24 +0100

    [upd] Header title and styles

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<title>Laravel</title>
+<title>Q&A</title>
 
 <!-- Styles -->
-<link rel="stylesheet" href="css/app.css">
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
