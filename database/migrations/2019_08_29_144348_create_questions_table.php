e95e85de42e3f6eba21616287fe4b939cf2b9a0d
Vasco Martins <vascomartins37@gmail.com>
2019-08-29 16:18:14 +0100

    [del] Default Migrations [add] create_questions_table

--- database/migrations/2019_08_29_144348_create_questions_table.php
+++ database/migrations/2019_08_29_144348_create_questions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreateQuestionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('questions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('question');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +27,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('questions');
     }
 }
